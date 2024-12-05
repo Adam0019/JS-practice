df50949915aec8dc7bd2fa385c49befff0bb279c
Adam0019 <99242657+Adam0019@users.noreply.github.com>
2024-12-05 11:56:16 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID = 145543
+let accountEmail = "aniruddha@gmail"
+var accountPass = "12346"
+accountCity = "Burdwan"
+
+//accountID = 2 // not allowed to change const value
+
+accountEmail ="hh@hh.com"
+
+accountPass = "489"
+
+accountCity = "kolkata"
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPass, accountCity]);
